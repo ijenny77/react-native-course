67cf923208df0e827fb083ff28cf04a4076a79db
unknown <ishimwejennymiriotta@gmail.com>
2026-04-14 18:15:39 +0200

    Pressable component

--- app/(auth)/login.jsx
+++ app/(auth)/login.jsx
@@ -1,13 +1,21 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet,Pressable,Text} from 'react-native'
 import ThemedView from '../../Components/ThemedView.jsx'
 import ThemedText from '../../Components/ThemedText.jsx'
 import Spacer from '../../Components/Spacer'
 import {Link} from 'expo-router'
+import Colors from '../../Constants/Colors.js'
+import ThemedButton from '../../Components/ThemedButton.jsx'
 function Login() {
+    const handleSubmit = () => {
+        
+    }
     return(
         <ThemedView style={styles.container}>
             <Spacer/>
             <ThemedText title={true} style={styles.title}>Login into your account</ThemedText>
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{color:'#f2f2f2'}}>Login</Text>
+            </ThemedButton>
             <Spacer height={100}/>
             <Link href='/register'>
             <ThemedText style={{textAlign:'center'}}>Register instead</ThemedText>
@@ -19,10 +27,19 @@ export default Login
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent:'center'
+        justifyContent:'center',
+        alignItems:'center'
     },
     title: {
         textAlign:"center",
         fontSize: 18,
+    },
+    btn:{
+        backgroundColor:Colors.primary,
+        padding:15,
+        borderRadius:5,
+    },
+    pressed:{
+        opacity:0.8
     }
 })
--- app/(auth)/register.jsx
+++ app/(auth)/register.jsx
@@ -1,15 +1,23 @@
-import {StyleSheet} from 'react-native'
+import {StyleSheet,Text} from 'react-native'
 import ThemedView from '../../Components/ThemedView.jsx'
 import ThemedText from '../../Components/ThemedText.jsx'
 import Spacer from '../../Components/Spacer'
+import ThemedButton from '../../Components/ThemedButton.jsx'
 import {Link} from 'expo-router'
+import Colors from '../../Constants/Colors.js'
 function Register() {
+    const handleSubmit = () => {
+
+    }
     return(
         <ThemedView style={styles.container}>
             <Spacer/>
             <ThemedText title={true} style={styles.title}>Register for an account</ThemedText>
+            <ThemedButton onPress={handleSubmit}>
+                <Text style={{color:'#f2f2f2'}}>Register</Text>
+            </ThemedButton>
             <Spacer height={100}/>
-            <Link href='/register'>
+            <Link href='/login'>
             <ThemedText style={{textAlign:'center'}}>Login instead</ThemedText>
             </Link>
         </ThemedView>
@@ -19,10 +27,19 @@ export default Register
 const styles = StyleSheet.create({
     container: {
         flex:1,
-        justifyContent:'center'
+        justifyContent:'center',
+        alignItems:'center'
     },
     title: {
         textAlign:"center",
         fontSize: 18,
+    },
+    btn:{
+        backgroundColor:Colors.primary,
+        padding:15,
+        borderRadius:5,
+    },
+    pressed:{
+        opacity:0.8
     }
 })
